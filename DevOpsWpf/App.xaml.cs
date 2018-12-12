32038f99b5eac4bac6012c0c188a5037d431a1be
Jon Patton <45438527+jonpattonweir@users.noreply.github.com>
2018-12-12 09:39:56 -0600

    test

--- DevOpsWpf/App.xaml.cs
+++ DevOpsWpf/App.xaml.cs
@@ -13,7 +13,7 @@ namespace DevOpsWpf
     /// </summary>
     public partial class App : Application
     {
-        int value = Int32.Parse("Test");
+        //int value = Int32.Parse("Test");
 
         //this is a second failure
     }
